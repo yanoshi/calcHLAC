8b39cc16d7c3a55e390e64be40dcb3452c2862d0
yanoshi <googreimu@gmail.com>
2015-06-04 18:46:48 +0900

    define HLAC Calculation method

---
+++ calcHLAC/hlac_calculator.cpp
@@ -0,0 +1,31 @@
+#include "hlac_calculator.h"
+
+using namespace std;
+using namespace cv;
+
+
+
+HLACCalculator::HLACCalculator(std::string input_path, std::shared_ptr<std::vector<cv::Rect>> calc_point)
+{
+
+}
+
+
+std::vector<int> HLACCalculator::get_result()
+{
+
+
+}
+
+
+std::string HLACCalculator::get_result_string()
+{
+
+}
+
+
+
+void HLACCalculator::get_result_file(std::string filepath)
+{
+	
+}
